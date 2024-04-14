fba53c8078ea711cf017462ea21c50039bec1153
Andre Luiz <andre2kluiz@gmail.com>
2024-04-13 22:29:13 -0300

    mudanças

--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -31,7 +31,14 @@ public class FormController {
         if(log != null && login.equals(log.getNome()) && senha.equals(log.getSenha())) {
             HttpSession session = request.getSession();
             session.setAttribute("logado", log.getNome());
-            response.sendRedirect("home.html" + "?msg=BemVindo" + log.getNome());
+            //response.sendRedirect("home.html" + "?msg=BemVindo" + log.getNome());
+            var writer = response.getWriter();
+
+            writer.println("<html>" + "<body>");
+            writer.println("<h1>Home - Lojista</h1>");
+            writer.println("<a href='/cadatroProduto'>Cadastra-Produto</a>" + "<br>" + "<br>");
+            writer.println("<a href='#'>Exibe-Produto</a>" + "<br>" + "<br>");
+            writer.println("<a href='/logout'>Deslogar</a>");
         }else {
             response.sendRedirect("index.html?msg=Loginfalhou");
         }
@@ -44,8 +51,8 @@ public class FormController {
         Boolean logado = (Boolean) session.getAttribute("logado");
 
         if (logado != null && logado) {
-            
-            response.sendRedirect("home.html");
+                       
+            response.sendRedirect("/home");
             
         } else {
             
@@ -59,6 +66,30 @@ public class FormController {
         session.invalidate();
 
         response.sendRedirect("index.html?msg=Usuario deslogado");
+    }
+
+    @RequestMapping("/lista")
+    public void listarProduto(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+    }
+
+    @RequestMapping(value = "/cadatroProduto", method = RequestMethod.GET)
+    public void cadastrarProduto(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        var writer = response.getWriter();
+        
+        writer.println("<html>" + "<head><link rel='stylesheet' href='/assets/styles/pages/index.css'></head>" + "<body>");
+        writer.println("<form action='/home' method='GET'>");
+        writer.println("<div class='input-box-II'>");
+        writer.println("<h1>Cadastro</h1>");
+        writer.println("<input type='text' placeholder='Nome' id='username' name='login' required>" + "<br>" + "<br>");
+        writer.println("<input type='text' placeholder='Descrição' id='username' name='login' required>"  + "<br>" + "<br>");
+        writer.println("<input type='number' placeholder='Preço' id='username' name='login' required>"  + "<br>" + "<br>");
+        writer.println("<input type='number' placeholder='Estoque' id='username' name='login' required>"  + "<br>" + "<br>");
+        writer.println("<button type='submit' class='btn'>Cadastrar</button>"  + "<br>" + "<br>");
+        writer.println("<div>");
+        writer.println("</form>");
+
+       
     }
 
 }
